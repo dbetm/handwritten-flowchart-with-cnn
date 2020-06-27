39d9fab3584c9e31b3762c70ae09362297d740ab
David <davbetm@gmail.com>
2020-06-27 00:26:31 -0500

    Shape model: Uncomment integration code

--- model/shape_model.py
+++ model/shape_model.py
@@ -115,49 +115,49 @@ if __name__ == '__main__':
 	"""Please, if you want to generate the report for a specified model,
 	uncomment the last block of code and comment first lines.
 	"""
-	# options_dict = get_options()
-	# print(options_dict)
-	#
-	# # Set default values
-	# if(options_dict['rois'] == None):
-	# 	options_dict['rois'] = 32
-	# if(options_dict['input_weight_path'] == None):
-	# 	options_dict['input_weight_path'] = "vgg16_weights_tf_dim_ordering_tf_kernels.h5"
-	# if(options_dict['epochs'] == None):
-	# 	options_dict['epochs'] = 5
-	# if(options_dict['learning_rate'] == None):
-	# 	options_dict['learning_rate'] = 1e-5
-	#
-	# shape_model = ShapeModel(
-	# 	dataset_path=options_dict['dataset'],
-	# 	num_rois=options_dict['rois'],
-	# 	weights_input_path=options_dict['input_weight_path']
-	# 	# weights_input_path="training_results/1/flowchart_3b_model.hdf5"
-	# 	# weights_input_path="vgg16_weights_tf_dim_ordering_tf_kernels.h5"
-	# )
-	# # testing train
-	# shape_model.train(
-	#     data_augmentation=False,
-	#     num_epochs=options_dict['epochs'],
-	# 	learning_rate=options_dict['learning_rate'],
-	# 	use_gpu=options_dict['gpu']
-	# )
+	options_dict = get_options()
+	print(options_dict)
+
+	# Set default values
+	if(options_dict['rois'] == None):
+		options_dict['rois'] = 32
+	if(options_dict['input_weight_path'] == None):
+		options_dict['input_weight_path'] = "vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+	if(options_dict['epochs'] == None):
+		options_dict['epochs'] = 5
+	if(options_dict['learning_rate'] == None):
+		options_dict['learning_rate'] = 1e-5
+
+	shape_model = ShapeModel(
+		dataset_path=options_dict['dataset'],
+		num_rois=options_dict['rois'],
+		weights_input_path=options_dict['input_weight_path']
+		# weights_input_path="training_results/1/flowchart_3b_model.hdf5"
+		# weights_input_path="vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+	)
+	# testing train
+	shape_model.train(
+	    data_augmentation=True,
+	    num_epochs=options_dict['epochs'],
+		learning_rate=options_dict['learning_rate'],
+		use_gpu=options_dict['gpu']
+	)
 
 	# -!-!- Temp: Unit testing for training without integration -!-!-
 
-	shape_model = ShapeModel(
-		dataset_path="/home/david/Escritorio/flowchart_3b_v3.1",
-		num_rois=32,
-		# weights_input_path="training_results/1/flowchart_3b_model.hdf5"
-		weights_input_path="vgg16_weights_tf_dim_ordering_tf_kernels.h5"
-	)
+	# shape_model = ShapeModel(
+	# 	dataset_path="/home/david/Escritorio/flowchart_3b_v3.1",
+	# 	num_rois=32,
+	# 	# weights_input_path="training_results/1/flowchart_3b_model.hdf5"
+	# 	weights_input_path="vgg16_weights_tf_dim_ordering_tf_kernels.h5"
+	# )
 	#testing train
-	shape_model.train(
-	    data_augmentation=True,
-	    num_epochs=1,
-		learning_rate=1e-5,
-		use_gpu=False
-	)
+	# shape_model.train(
+	#     data_augmentation=True,
+	#     num_epochs=1,
+	# 	learning_rate=1e-5,
+	# 	use_gpu=False
+	# )
 
 	# shape_model.generate_classification_report(
 	# 	results_path = "training_results/5/",
